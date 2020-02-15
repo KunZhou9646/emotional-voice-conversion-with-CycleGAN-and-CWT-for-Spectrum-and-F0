81d25b6f1728a2e1838b8295e1030ce0e1ee6694
KunZhou9646 <59918673+KunZhou9646@users.noreply.github.com>
2020-02-15 12:48:46 +0800

    Add files via upload

---
+++ Parallel-data-free emotional voice conversion with CycleGAN and CWT/train_f0.py
@@ -0,0 +1,184 @@
+import os
+import numpy as np
+import argparse
+import time
+import librosa
+from sklearn import preprocessing
+from preprocess import *
+from model_f0 import CycleGAN
+from utils import get_lf0_cwt_norm
+from utils import get_cont_lf0, get_lf0_cwt,inverse_cwt
+
+def train(train_A_dir, train_B_dir, model_dir, model_name, random_seed, validation_A_dir, validation_B_dir, output_dir, tensorboard_log_dir):
+# 10-scale F0 + 24 MCEPs:
+    np.random.seed(random_seed)
+    num_epochs = 5000
+    mini_batch_size = 1 # mini_batch_size = 1 is better
+    generator_learning_rate = 0.0002
+    generator_learning_rate_decay = generator_learning_rate / 200000
+    discriminator_learning_rate = 0.0001
+    discriminator_learning_rate_decay = discriminator_learning_rate / 200000
+    sampling_rate = 16000
+    num_mcep = 24
+    num_scale = 10
+    frame_period = 5.0
+    n_frames = 128
+    lambda_cycle = 10
+    lambda_identity = 5
+
+    print('Preprocessing Data...')
+
+    start_time = time.time()
+
+    wavs_A = load_wavs(wav_dir = train_A_dir, sr = sampling_rate)
+    wavs_B = load_wavs(wav_dir = train_B_dir, sr = sampling_rate)
+
+    f0s_A, timeaxes_A, sps_A, aps_A, coded_sps_A = world_encode_data(wavs = wavs_A, fs = sampling_rate, frame_period = frame_period, coded_dim = num_mcep)
+    f0s_B, timeaxes_B, sps_B, aps_B, coded_sps_B = world_encode_data(wavs = wavs_B, fs = sampling_rate, frame_period = frame_period, coded_dim = num_mcep)
+
+    log_f0s_mean_A, log_f0s_std_A = logf0_statistics(f0s_A)
+    log_f0s_mean_B, log_f0s_std_B = logf0_statistics(f0s_B)
+    #############################
+    #get lf0 cwt:
+    lf0_cwt_norm_A, scales_A, means_A, stds_A = get_lf0_cwt_norm(f0s_A, mean = log_f0s_mean_A, std = log_f0s_std_A)
+    lf0_cwt_norm_B, scales_B, means_B, stds_B = get_lf0_cwt_norm(f0s_B, mean = log_f0s_mean_B, std = log_f0s_std_B)
+
+
+    print('Log Pitch A')
+    print('Mean: %f, Std: %f' %(log_f0s_mean_A, log_f0s_std_A))
+    print('Log Pitch B')
+    print('Mean: %f, Std: %f' %(log_f0s_mean_B, log_f0s_std_B))
+
+    lf0_cwt_norm_A_transposed = transpose_in_list(lst = lf0_cwt_norm_A)
+    lf0_cwt_norm_B_transposed = transpose_in_list(lst = lf0_cwt_norm_B)
+
+    coded_sps_A_transposed = transpose_in_list(lst = coded_sps_A)
+    coded_sps_B_transposed = transpose_in_list(lst = coded_sps_B)
+
+    coded_sps_A_norm, coded_sps_A_mean, coded_sps_A_std = coded_sps_normalization_fit_transoform(coded_sps = coded_sps_A_transposed)
+    coded_sps_B_norm, coded_sps_B_mean, coded_sps_B_std = coded_sps_normalization_fit_transoform(coded_sps = coded_sps_B_transposed)
+
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    np.savez(os.path.join(model_dir, 'logf0s_normalization.npz'), mean_A = log_f0s_mean_A, std_A = log_f0s_std_A, mean_B = log_f0s_mean_B, std_B = log_f0s_std_B)
+    np.savez(os.path.join(model_dir, 'mcep_normalization.npz'), mean_A = coded_sps_A_mean, std_A = coded_sps_A_std, mean_B = coded_sps_B_mean, std_B = coded_sps_B_std)
+
+    if validation_A_dir is not None:
+        validation_A_output_dir = os.path.join(output_dir, 'converted_A')
+        if not os.path.exists(validation_A_output_dir):
+            os.makedirs(validation_A_output_dir)
+
+    if validation_B_dir is not None:
+        validation_B_output_dir = os.path.join(output_dir, 'converted_B')
+        if not os.path.exists(validation_B_output_dir):
+            os.makedirs(validation_B_output_dir)
+
+    end_time = time.time()
+    time_elapsed = end_time - start_time
+
+    print('Preprocessing Done.')
+
+    print('Time Elapsed for Data Preprocessing: %02d:%02d:%02d' % (time_elapsed // 3600, (time_elapsed % 3600 // 60), (time_elapsed % 60 // 1)))
+    
+    num_feats = 10 #34
+    #model = CycleGAN(num_features = num_feats)
+    #model.load('./model/neutral2anger_f0/neutral2anger_f0.ckpt')
+    #print('model restored')
+
+    for epoch in range(num_epochs):
+        print('Epoch: %d' % epoch)
+        '''
+        if epoch > 60:
+            lambda_identity = 0
+        if epoch > 1250:
+            generator_learning_rate = max(0, generator_learning_rate - 0.0000002)
+            discriminator_learning_rate = max(0, discriminator_learning_rate - 0.0000001)
+        '''
+
+        start_time_epoch = time.time()
+        ###zk
+        data_As = list()
+        data_Bs = list()
+        for lf0_a in lf0_cwt_norm_A_transposed:
+            data_A = lf0_a
+            data_As.append(data_A)
+
+        for lf0_b in lf0_cwt_norm_B_transposed:
+            data_B = lf0_b
+            data_Bs.append(data_B)
+#        for sp_a, lf0_a in zip(coded_sps_A_norm,lf0_cwt_norm_A_transposed):
+#            data_A = np.vstack((sp_a,lf0_a))
+#            data_As.append(data_A)
+#        for sp_b, lf0_b in zip(coded_sps_B_norm,lf0_cwt_norm_B_transposed):
+#            data_B = np.vstack((sp_b,lf0_b))
+#            data_Bs.append(data_B)
+
+        #dataset_A, dataset_B = sample_train_data(dataset_A = coded_sps_A_norm, dataset_B = coded_sps_B_norm, n_frames = n_frames)
+        dataset_A, dataset_B = sample_train_data(dataset_A = data_As, dataset_B = data_Bs, n_frames = n_frames)
+
+        n_samples = dataset_A.shape[0]
+
+        for i in range(n_samples // mini_batch_size):
+
+            num_iterations = n_samples // mini_batch_size * epoch + i
+
+            if num_iterations > 10000:
+                lambda_identity = 0
+            if num_iterations > 200000:
+                generator_learning_rate = max(0, generator_learning_rate - generator_learning_rate_decay)
+                discriminator_learning_rate = max(0, discriminator_learning_rate - discriminator_learning_rate_decay)
+
+            start = i * mini_batch_size
+            end = (i + 1) * mini_batch_size
+
+            generator_loss, discriminator_loss = model.train(input_A = dataset_A[start:end], input_B = dataset_B[start:end], lambda_cycle = lambda_cycle, lambda_identity = lambda_identity, generator_learning_rate = generator_learning_rate, discriminator_learning_rate = discriminator_learning_rate)
+
+            if i % 50 == 0:
+                #print('Iteration: %d, Generator Loss : %f, Discriminator Loss : %f' % (num_iterations, generator_loss, discriminator_loss))
+                print('Iteration: {:07d}, Generator Learning Rate: {:.7f}, Discriminator Learning Rate: {:.7f}, Generator Loss : {:.3f}, Discriminator Loss : {:.3f}'.format(num_iterations, generator_learning_rate, discriminator_learning_rate, generator_loss, discriminator_loss))
+
+        model.save(directory = model_dir, filename = model_name)
+
+        end_time_epoch = time.time()
+        time_elapsed_epoch = end_time_epoch - start_time_epoch
+
+        print('Time Elapsed for This Epoch: %02d:%02d:%02d' % (time_elapsed_epoch // 3600, (time_elapsed_epoch % 3600 // 60), (time_elapsed_epoch % 60 // 1)))
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description = 'Train CycleGAN model for datasets.')
+
+    train_A_dir_default = './data/training/NEUTRAL'
+    train_B_dir_default = './data/training/SURPRISE'
+    model_dir_default = './model/neutral_surprise_f0'
+    model_name_default = 'neutral_to_surprise_f0.ckpt'
+    random_seed_default = 0
+    validation_A_dir_default = './data/evaluation_all/NEUTRAL'
+    validation_B_dir_default = './data/evaluation_all/SURPRISE'
+    output_dir_default = './validation_output'
+    tensorboard_log_dir_default = './log'
+
+    parser.add_argument('--train_A_dir', type = str, help = 'Directory for A.', default = train_A_dir_default)
+    parser.add_argument('--train_B_dir', type = str, help = 'Directory for B.', default = train_B_dir_default)
+    parser.add_argument('--model_dir', type = str, help = 'Directory for saving models.', default = model_dir_default)
+    parser.add_argument('--model_name', type = str, help = 'File name for saving model.', default = model_name_default)
+    parser.add_argument('--random_seed', type = int, help = 'Random seed for model training.', default = random_seed_default)
+    parser.add_argument('--validation_A_dir', type = str, help = 'Convert validation A after each training epoch. If set none, no conversion would be done during the training.', default = validation_A_dir_default)
+    parser.add_argument('--validation_B_dir', type = str, help = 'Convert validation B after each training epoch. If set none, no conversion would be done during the training.', default = validation_B_dir_default)
+    parser.add_argument('--output_dir', type = str, help = 'Output directory for converted validation voices.', default = output_dir_default)
+    parser.add_argument('--tensorboard_log_dir', type = str, help = 'TensorBoard log directory.', default = tensorboard_log_dir_default)
+
+    argv = parser.parse_args()
+
+    train_A_dir = argv.train_A_dir
+    train_B_dir = argv.train_B_dir
+    model_dir = argv.model_dir
+    model_name = argv.model_name
+    random_seed = argv.random_seed
+    validation_A_dir = None if argv.validation_A_dir == 'None' or argv.validation_A_dir == 'none' else argv.validation_A_dir
+    validation_B_dir = None if argv.validation_B_dir == 'None' or argv.validation_B_dir == 'none' else argv.validation_B_dir
+    output_dir = argv.output_dir
+    tensorboard_log_dir = argv.tensorboard_log_dir
+
+    train(train_A_dir = train_A_dir, train_B_dir = train_B_dir, model_dir = model_dir, model_name = model_name, random_seed = random_seed, validation_A_dir = validation_A_dir, validation_B_dir = validation_B_dir, output_dir = output_dir, tensorboard_log_dir = tensorboard_log_dir)
